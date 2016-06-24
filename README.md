fd38a5f3a7cf1a8b277283c54fdca2a6280bb489
Jason Kwong <kwongj@gmail.com>
2016-06-25 09:46:41 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,142 @@
-# meningotype
-In silico serotyping and finetyping (porA and fetA) of Neisseria meningitidis
+#meningotype
+
+*In silico* serotyping and finetyping (*porA* and *fetA*) of *Neisseria meningitidis*  
+
+##Authors
+
+* Jason Kwong (@kwongjc)
+* Anders Gonçalves da Silva
+* Torsten Seemann (@torstenseemann)
+
+##Dependencies
+
+* [Python 2.7.x](https://www.python.org/)
+* [BioPython](http://biopython.org/)
+* [isPcr v33](http://hgwdev.cse.ucsc.edu/~kent/src/) by Jim Kent
+
+##Installation
+
+To install:
+
+```
+$ git clone https://github.com/MDU-PHL/meningotype
+```
+
+### To test installation
+
+Once installed, you can run the following to ensure `meningotype` is successfully working:
+
+    ```$ meningotype.py --test```
+
+If everything works, you will see the following:
+
+```
+$ meningotype.py --test
+Running meningotype.py on test examples ... 
+$ meningotype.py A.fna B.fna C.fna W.fna X.fna Y.fna
+SAMPLE_ID	SEROTYPE	PorA_TYPE	FetA_TYPE
+test/A.fna	A
+test/B.fna	B
+test/C.fna	C
+test/W.fna	W
+test/X.fna	X
+test/Y.fna	Y
+```
+
+or, to check finetyping:
+
+```
+$ meningotype.py --test --finetype
+Running meningotype.py on test examples ... 
+$ meningotype.py A.fna B.fna C.fna W.fna X.fna Y.fna
+SAMPLE_ID	SEROTYPE	PorA_TYPE	FetA_TYPE
+test/A.fna	A	7,13-1		F1-5
+test/B.fna	B	5-2,10-1	F3-6
+test/C.fna	C	21,26-2		F1-5
+test/W.fna	W	5,2			F1-1
+test/X.fna	X	5-1,10-1	F4-23
+test/Y.fna	Y	5-2,10-1	F4-1
+```
+
+
+##Usage
+
+```	$ meningotype.py -h
+
+	usage: 
+	  meningotype.py [OPTIONS] <fasta1> <fasta2> <fasta3> ... <fastaN>
+	
+	In silico typing for Neisseria meningitidis
+	
+	PCR Serotyping Ref: Mothershed et al, J Clin Microbiol 2004; 42(1): 320-328
+	
+	porA and fetA typing Ref: Jolley et al, FEMS Microbiol Rev 2007; 31: 89-96
+	See also http://www.neisseria.org/nm/typing/
+	
+	positional arguments:
+	  FASTA       input FASTA files eg. fasta1, fasta2, fasta3 ... fastaN
+	
+	optional arguments:
+	  -h, --help  show this help message and exit
+	  --finetype  Perform porA and fetA fine typing (default=off)
+	  --db DB     Specify custom directory containing allele databases for porA/fetA typing
+	              Directory must contain database files "FetA_VR.fas", "PorA_VR1.fas", and "PorA_VR2.fas"
+	  --printseq  Save porA, porB and fetA allele sequences to file (default=off)
+	  --updatedb  update allele database from <www.ng-mast.net>
+	  --test      run test example
+	  --version   show program's version number and exit
+```
+
+##Quick start
+
+**To perform *in silico* serotyping on FASTA files:**
+
+`$ meningotype.py <fasta1> <fasta2> <fasta3> ... <fastaN>`
+
+The serotypes are printed in tab-separated format to `stdout`.
+
+**To save results to a tab-separated text file, redirect `stdout`:**
+
+`$ meningotype.py <fasta1> <fasta2> <fasta3> ... <fastaN>  > results.txt`
+
+**To perform *in silico* serotyping AND finetyping of the porA and fetA genes:
+
+`$ meningotype.py --finetype <fasta1> <fasta2> <fasta3> ... <fastaN>`
+
+**To save finetyping sequences of the alleles to a file (eg. for uploading "new" sequences to [http://pubmlst.org/neisseria/](http://pubmlst.org/neisseria/)):**
+
+`$ meningotype.py --finetype --printseq <fasta1> <fasta2> <fasta3> ... <fastaN>`
+
+##Updating the allele databases
+
+**To update the allele databases from http://pubmlst.org/neisseria/ :**  
+*Warning: Ensure you back up your old databases if you wish to keep them.*
+
+	$ meningotype.py --updatedb
+
+A copy of the old database is saved just in case, but is overwritten with each subsequent   ```--updatedb```.
+
+
+##Citation
+
+Please cite as:
+
+Kwong JC, Gonçalves da Silva A, Stinear TP, Howden BP and Seemann T.  
+***meningotype*: *in silico* typing for *Neisseria meningitidis*.**  
+GitHub https://github.com/MDU-PHL/meningotype
+
+##Bugs
+
+Please submit via the [GitHub issues page](https://github.com/MDU-PHL/meningotype/issues).  
+
+Note that the finetyping databases and website are curated and hosted by http://pubmlst.org/neisseria/. For issues with the databases, please contact the pubmlst curator](mailto:keith.jolley@zoo.ox.ac.uk).
+
+##Software Licence
+
+[GPLv3](https://github.com/MDU-PHL/meningotype/blob/master/LICENSE)
+
+##References
+
+* Mothershed et al. J Clin Microbiol, 2004; 42(1): 320-328.
+* Jolley et al. FEMS Microbiol Rev, 2007; 31: 89-96.
+* See also [http://www.neisseria.org/nm/typing/](http://www.neisseria.org/nm/typing/).
---
+++ meningotype.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python
+# Script by Jason Kwong
+# In silico typing for Neisseria meningitidis
+
+# Use modern print function from python 3.x
+from __future__ import print_function
+
+# Usage
+import argparse
+from argparse import RawTextHelpFormatter
+import sys
+import os
+import os.path
+import StringIO
+import urllib
+import subprocess
+from subprocess import Popen
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Blast.Applications import NcbiblastnCommandline
+from Bio.Blast.Applications import NcbiblastxCommandline
+
+# Standard functions
+# Log a message to stderr
+def msg(*args, **kwargs):
+	print(*args, file=sys.stderr, **kwargs)
+
+# Log an error to stderr and quit with non-zero error code
+def err(*args, **kwargs):
+	msg(*args, **kwargs)
+	sys.exit(1);
+
+# Format database
+def format(file):
+	sed_all(file, '^.*<textarea name="concatenation".*?>', '')
+	sed_all(file, '<\/textarea>.*$', '')
+	sed_inplace(file, '&gt;', '>')
+
+parser = argparse.ArgumentParser(
+	formatter_class=RawTextHelpFormatter,
+	description='In silico typing for Neisseria meningitidis\n'
+		'\nPCR Serotyping Ref: Mothershed et al, J Clin Microbiol 2004; 42(1): 320-328\n'
+		'\nporA and fetA typing Ref: Jolley et al, FEMS Microbiol Rev 2007; 31: 89-96\n'
+		'See also http://www.neisseria.org/nm/typing/',
+	usage='\n  %(prog)s [OPTIONS] <fasta1> <fasta2> <fasta3> ... <fastaN>')
+parser.add_argument('fasta', metavar='FASTA', nargs='*', help='input FASTA files eg. fasta1, fasta2, fasta3 ... fastaN')
+# CSV option excluded due to syntax of porA finetype VR1,VR2
+#parser.add_argument('--csv', action='store_true', default=False, help='output comma-separated format (CSV) rather than tab-separated')
+parser.add_argument('--finetype', action='store_true', help='Perform porA and fetA fine typing (default=off)')
+parser.add_argument('--db', metavar='DB', help='Specify custom directory containing allele databases for porA/fetA typing\n'
+	'Directory must contain database files "FetA_VR.fas", "PorA_VR1.fas", and "PorA_VR2.fas"')
+parser.add_argument('--printseq', action='store_true', help='Save porA, porB and fetA allele sequences to file (default=off)')
+parser.add_argument('--updatedb', action='store_true', default=False, help='update allele database from <www.ng-mast.net>')
+parser.add_argument('--test', action='store_true', default=False, help='run test example')
+parser.add_argument('--version', action='version', version=
+	'=====================================\n'
+	'%(prog)s v0.1-alpha\n'
+	'Updated 21-Jun-2016 by Jason Kwong\n'
+	'Dependencies: isPcr, BLAST, BioPython\n'
+	'=====================================')
+args = parser.parse_args()
+
+if args.db:
+	DBpath = str(args.db).rstrip('/')
+else:
+	DBpath = sys.path[0] + '/db'
+
+# URLs to update database files
+porA1URL = 'http://pubmlst.org/perl/bigsdb/bigsdb.pl?db=pubmlst_neisseria_seqdef&page=downloadAlleles&locus=PorA_VR1'
+porA2URL = 'http://pubmlst.org/perl/bigsdb/bigsdb.pl?db=pubmlst_neisseria_seqdef&page=downloadAlleles&locus=PorA_VR2'
+fetAURL = 'http://pubmlst.org/perl/bigsdb/bigsdb.pl?db=pubmlst_neisseria_seqdef&page=downloadAlleles&locus=FetA_VR'
+
+# Path to database files
+porA1alleles = DBpath + '/PorA_VR1.fas'
+porA2alleles = DBpath + '/PorA_VR2.fas'
+fetalleles = DBpath + '/FetA_VR.fas'
+seroALLELES = DBpath + '/seroALLELES.fa'
+allelesDB = DBpath + '/blast/seroALLELES'
+seroPRIMERS = DBpath + '/seroPRIMERS'
+finetypePRIMERS = DBpath + '/finetypePRIMERS'
+porA1DB = DBpath + '/blast/porA1'
+porA2DB = DBpath + '/blast/porA2'
+fetDB = DBpath + '/blast/fet'
+
+# Update database files
+def update_db(db_file, db_url):
+	if os.path.isfile(db_file):
+		os.rename(db_file, db_file+'.old')
+	urllib.urlretrieve(db_url, db_file)
+
+if args.updatedb:
+	msg('Updating "{}" ... '.format(porA1alleles))
+	update_db(porA1alleles, porA1URL)
+	msg('Updating "{}" ... '.format(porA2alleles))
+	update_db(porA2alleles, porA2URL)
+	msg('Updating "{}" ... '.format(fetalleles))
+	update_db(fetalleles, fetAURL)
+	msg('Done.')
+	sys.exit(0)
+
+# Check database files are present
+def check_primer_files(f):
+	if not os.path.isfile(f):
+		err('ERROR: Cannot locate file: "{}"'.format(f))
+
+def check_db_files(f, db_url):
+	if not os.path.isfile(f):
+		update_db(f, db_url)
+
+if not os.path.exists(DBpath):
+	err('ERROR: Cannot locate "db" directory at "{}"'.format(DBpath))
+check_primer_files(seroPRIMERS)
+check_primer_files(seroALLELES)
+check_primer_files(finetypePRIMERS)
+check_db_files(porA1alleles, porA1URL)
+check_db_files(porA2alleles, porA2URL)
+check_db_files(fetalleles, fetAURL)
+
+# Set up BLAST databases if not present
+def makeblastDB(db, infile, dbtype):
+	if dbtype == 'nucl':
+		DBindex = db + '.nin'
+	elif dbtype == 'prot':
+		DBindex = db + '.pin'
+	if not os.path.isfile(DBindex):
+		proc = subprocess.Popen(['makeblastdb', '-in', infile, '-out', db, '-dbtype', dbtype], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+makeblastDB(allelesDB, seroALLELES, 'nucl')
+makeblastDB(porA1DB, porA1alleles, 'prot')
+makeblastDB(porA2DB, porA2alleles, 'prot')
+makeblastDB(fetDB, fetalleles, 'prot')
+
+# Test example to check meningotype works
+if args.test:
+	TESTpath = sys.path[0] + '/test/'
+	testSEQS = [TESTpath+'A.fna', TESTpath+'B.fna', TESTpath+'C.fna', TESTpath+'W.fna', TESTpath+'X.fna', TESTpath+'Y.fna']
+	msg('Running meningotype.py on test examples ... ')
+	msg('$ meningotype.py A.fna B.fna C.fna W.fna X.fna Y.fna')
+	args.fasta = testSEQS
+
+# Create folder for output sequences if specified
+if args.printseq:
+	try:
+		if not os.path.exists('printseq'):
+		    os.makedirs('printseq')
+		else:
+			err('"printseq" folder already exists in this directory.')
+	except:
+		err('Unable to create "printseq" folder in this directory.')
+
+# Set global values and dictionaries
+alleleSIZE = {'A':92, 'B':169, 'C':74, 'W':129, 'X':65, 'Y':146}
+seroDICT = {'sacB':'A', 'synD':'B', 'synE':'C', 'synG':'W', 'xcbB':'X', 'synF':'Y'}
+
+porASEQS = []
+fetASEQS = []
+sero = None
+porA = None
+fet = None
+sep = '\t'
+
+# Functions
+def seroTYPE(f):
+	seroCOUNT = []				# Setup list in case there are mixed/multiple hits
+	proc = subprocess.Popen(['isPcr', f, seroPRIMERS, 'stdout', '-minPerfect=10'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	PCRout = proc.communicate()[0]
+	if not PCRout:
+		sero = None
+		seroBLAST = NcbiblastnCommandline(query=f, db=allelesDB, task='blastn', perc_identity=90, evalue='1e-20', outfmt='"6 sseqid pident length"')
+		stdout, stderr = seroBLAST()
+		lenMATCH = 0
+		line = stdout.split('\n')[0]
+		amp = line.split('\t')
+#		ampID = amp[0]
+#		ampPERC = amp[1]
+#		ampLEN = int(amp[2])
+		sero = amp[0]		# Currently only takes top/first BLAST hit
+		if not sero:
+			seroCOUNT.append('-')
+		else:
+			seroCOUNT.append(sero)
+	else:
+		alleleSEQ = StringIO.StringIO()
+		alleleSEQ.write(PCRout)
+		alleleSEQ.seek(0)
+		for amplicon in SeqIO.parse(alleleSEQ, "fasta"):
+			product = amplicon.description.split()
+			ampID = product[1]
+			ampLEN = int(product[2][:-2])
+			sero = seroDICT[ampID]
+			expLEN = int(alleleSIZE[sero])
+			if ampLEN > (expLEN-6) and ampLEN < (expLEN+6):
+				seroCOUNT.append(sero)
+		alleleSEQ.close()
+	return seroCOUNT
+
+def finetypeBLAST(s, db):
+	ft = None
+	allele = None
+	ftBLAST = NcbiblastxCommandline(query='-', db=db, outfmt='"6 qseqid sseqid pident length gaps"', seg='no')
+	stdout, stderr = ftBLAST(stdin=s.format('fasta'))
+	if stdout:
+		BLASTout = stdout.split('\n')
+		lenMATCH = 0
+		for line in BLASTout:
+			if line.strip():
+				BLASTline = line.split('\t')
+				if float(BLASTline[2]) == 100 and BLASTline[4] == '0':
+					if int(BLASTline[3]) > int(lenMATCH):
+						lenMATCH = BLASTline[3]
+						ftRESULT = BLASTline[1]
+						ft = ftRESULT.split('_')[2]
+				elif not ft:
+					ft = 'new'
+	return str(ft)
+
+def fineTYPE(f):
+	porACOUNT = []				# Setup list in case there are mixed/multiple hits
+	fetACOUNT = []
+	global porASEQS
+	global fetASEQS
+	proc = subprocess.Popen(['isPcr', f, finetypePRIMERS, 'stdout', '-maxSize=800', '-tileSize=10', '-minPerfect=8', '-stepSize=3'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+	PCRout = proc.communicate()[0]
+	alleleSEQ = StringIO.StringIO()
+	alleleSEQ.write(PCRout)
+	alleleSEQ.seek(0)
+	for amplicon in SeqIO.parse(alleleSEQ, "fasta"):
+		ampFILE = amplicon.description.split()
+		ampID = ampFILE[1]
+		ampLEN = ampFILE[2]					# Need to check amplicon length to exclude double hits?
+		if ampID == 'porA':
+			A1 = finetypeBLAST(amplicon, porA1DB)
+			A2 = finetypeBLAST(amplicon, porA2DB)
+			porA = A1 + ',' + A2
+			porACOUNT.append(porA)
+			porASEQ = amplicon.seq.upper()
+			porARECR = SeqRecord(porASEQ, id=f, description='PorA')
+			porASEQS.append(porARECR)
+		if ampID == 'fetA':
+			fet = finetypeBLAST(amplicon, fetDB)
+			fetACOUNT.append(fet)
+			fetASEQ = amplicon.seq.upper()
+			fetARECR = SeqRecord(fetASEQ, id=f, description='FetA')
+			fetASEQS.append(fetARECR)
+	if len(porACOUNT) == 0:
+		porACOUNT.append('-')
+	if len(fetACOUNT) == 0:
+		fetACOUNT.append('-')
+	return porACOUNT, fetACOUNT
+
+def results(f, seroCOUNT, porCOUNT, fetCOUNT):
+	seroTYPE = '/'.join(seroCOUNT)
+	if not porCOUNT:
+		print(f + sep + seroTYPE)
+	else:
+		porTYPE = '/'.join(porCOUNT)
+		fetTYPE = '/'.join(fetCOUNT)
+		print(f + sep + seroTYPE + sep + porTYPE + sep + fetTYPE)
+
+# Run meningotype
+print('SAMPLE_ID' + sep + 'SEROTYPE' + sep + 'PorA_TYPE' + sep + 'FetA_TYPE')
+for f in args.fasta:
+	seroCOUNT = seroTYPE(f)
+	if not args.finetype:
+		results(f, seroCOUNT, None, None)
+	else:
+		ftRESULTS = fineTYPE(f)
+		porACOUNT = ftRESULTS[0]
+		fetACOUNT = ftRESULTS[1]
+		results(f, seroCOUNT, porACOUNT, fetACOUNT)
+
+# Print allele sequences to file
+if args.printseq:
+	with open('printseq/porA_seqs.fasta', 'w') as output:
+		SeqIO.write(porASEQS, output, 'fasta')
+	with open('printseq/fetA_seqs.fasta', 'w') as output:
+		SeqIO.write(fetASEQS, output, 'fasta')
+
+sys.exit(0)
