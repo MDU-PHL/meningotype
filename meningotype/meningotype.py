5e08cd77f0668300917443bfd839150f185b6e81
Jason Kwong <kwongj@gmail.com>
2017-02-22 16:46:37 +1100

    Added check for ctrA

--- meningotype/meningotype.py
+++ meningotype/meningotype.py
@@ -11,7 +11,6 @@ from argparse import RawTextHelpFormatter
 import sys
 import os
 import os.path
-import inspect
 import StringIO
 import urllib
 import subprocess
@@ -24,17 +23,10 @@ from Bio.Blast.Applications import NcbiblastxCommandline
 from pkg_resources import resource_string, resource_filename
 
 # Import local modules
-# realpath() will make your script run, even if you symlink it :)
-cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
-if cmd_folder not in sys.path:
-	sys.path.insert(0, cmd_folder)
-
-# use this if you want to include modules from a subfolder
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"scripts")))
-if cmd_subfolder not in sys.path:
-	sys.path.insert(0, cmd_subfolder)
-
-import scripts.nmen as nmen
+scripts = resource_filename(__name__, 'scripts')
+if scripts not in sys.path:
+	sys.path.insert(0, scripts)
+import nmen
 import menwy
 import ctrA
 
