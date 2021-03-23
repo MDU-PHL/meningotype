ba7850690e9c6bcd78b4099eaf10878341de615e
Anders Goncalves da Silva <andersgs@gmail.com>
2021-03-23 16:48:06 -0700

    Fix version tracking
    Make it cannonical with a version string in init file

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup
+from meningotype import __version__ as version
+
 
 def readme():
 	with open('README.md') as f:
 		return f.read()
 
-def current_version():
-	with open('meningotype/meningotype.py') as f:
-		for index, line in enumerate(f):
-			if index == 8:
-				return line.split("'")[1]
 
 setup(name='meningotype',
-	version=current_version(),
+	version=version,
 	description='In silico serotyping and finetyping (porA and fetA) of Neisseria meningitidis',
 	long_description=readme(),
 	classifiers=[
 		'Development Status :: 4 - Beta',
 		'License :: OSI Approved :: GPLv3',
-		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+		'Programming Language :: Python :: 3.9',
 		'Topic :: Scientific/Engineering :: Bio-Informatics',
 		'Topic :: Scientific/Engineering :: Medical Science Apps.',
 		'Intended Audience :: Science/Research',
