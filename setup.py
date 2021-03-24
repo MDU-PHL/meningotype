8e7410cdfc2b2ec5a12ba7b8815067500917c78e
Anders Goncalves da Silva <andersgs@gmail.com>
2021-03-23 17:13:41 -0700

    Fix license classifier

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='meningotype',
 	long_description=readme(),
 	classifiers=[
 		'Development Status :: 4 - Beta',
-		'License :: OSI Approved :: GPLv3',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 		'Programming Language :: Python :: 3.6',
 		'Programming Language :: Python :: 3.7',
 		'Programming Language :: Python :: 3.8',
